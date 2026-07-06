b427ffe1320aed93387cbdde773db3dd1d416ea4
pasichnykTetiana-create <pasichnyktetiana.de@gmail.com>
2026-07-06 20:43:11 +0200

    Fix hero full-screen layout and remove white gap before Skills

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,13 @@
 import MyHeader from '../Header/Header'
 import Skills from '../Skills/Skills'
+import style from './Home.module.css'
 
 const MyHome = () => {
 	return (
-		<>
+		<div className={style.home}>
 			<MyHeader />
 			<Skills />
-		</>
+		</div>
 	)
 }
 
